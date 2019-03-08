fb329b13431392883c48955b9d6523cf49f7f496
刘富鸿 <kurenai233@yahoo.com>
2019-03-08 12:00:21 +0800

    chore(springGuides): 增加guide代码

---
+++ src/main/java/org/kurenai/springguides/Receiver.java
@@ -0,0 +1,30 @@
+package org.kurenai.springguides;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.CountDownLatch;
+
+/**
+ * Class Name: Receiver
+ * Description:
+ *
+ * @author: liufuhong
+ * @mail: liufuhong@ly-sky.com
+ * @date: 2019年03月08日
+ * @version: 1.0
+ */
+
+@Component
+public class Receiver {
+
+    private CountDownLatch latch = new CountDownLatch(1);
+
+    public void receiveMessage(String message) {
+        System.out.println("Received <" + message + ">");
+        latch.countDown();
+    }
+
+    public CountDownLatch getLatch() {
+        return latch;
+    }
+}
---
+++ src/main/java/org/kurenai/springguides/SpringGuidesApplication.java
@@ -0,0 +1,63 @@
+package org.kurenai.springguides;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
+import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+/**
+ * Class Name: SpringGuidesApplication
+ * Description:
+ *
+ * @author: liufuhong
+ * @mail: liufuhong@ly-sky.com
+ * @date: 2019年03月08日
+ * @version: 1.0
+ */
+
+@SpringBootApplication
+public class SpringGuidesApplication {
+    private static final String topicExchangeName = "spring-boot-exchange";
+
+    private static final String queueName = "spring-boot";
+
+    @Bean
+    Queue queue() {
+        return new Queue(queueName, false);
+    }
+
+    @Bean
+    TopicExchange exchange() {
+        return new TopicExchange(topicExchangeName);
+    }
+
+    @Bean
+    Binding binding(Queue queue, TopicExchange exchange) {
+        return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+    }
+
+    @Bean
+    SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
+                                             MessageListenerAdapter listenerAdapter) {
+        SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
+        container.setConnectionFactory(connectionFactory);
+        container.setQueueNames(queueName);
+        container.setMessageListener(listenerAdapter);
+        return container;
+    }
+
+    @Bean
+    MessageListenerAdapter listenerAdapter(Receiver receiver) {
+        return new MessageListenerAdapter(receiver, "receiveMessage");
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        SpringApplication.run(SpringGuidesApplication.class, args).close();
+    }
+}
